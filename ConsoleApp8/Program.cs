1438d9ce23448967f960e621ad1f4bd26cf80b01
visual studio <vs@itstep.ge>
2023-10-21 19:58:18 +0400

    Add project files.

---
+++ ConsoleApp8/Program.cs
@@ -0,0 +1,14 @@
+﻿string[] websites =
+            {
+            "hsNameA.com", "hsNameB.net", "hsNameC.net",
+            "hsNameD.com", "hsNameE.org", "hsNameF.org",
+            "hsNameG.tv", "hsNameH.net", "hsNameI.tv"
+};
+
+var query = from i in websites
+            select i.Split('.').Last();
+
+foreach(var item in query)
+{
+    Console.WriteLine(item);
+}
